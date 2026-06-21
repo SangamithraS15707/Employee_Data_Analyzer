bc6f3bd83c02e36b397262681729815ea26893a1
SangamithraS15707 <sangamithra15707@gmail.com>
2026-06-21 13:37:27 +0530

    Implemented Employee and EmployeeManager Classes

---
+++ src/EmployeeManager.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class EmployeeManager {
+    private Map<Integer, Employee> employees;
+    public EmployeeManager() {
+        employees = new HashMap<>();
+    }
+    public void addEmployee(Employee employee) {
+        employees.put(employee.getId(), employee);
+    }
+    public Optional<Employee> getEmployeeById(int id) {
+        return Optional.ofNullable(employees.get(id));
+    }
+
+    public List<Employee> getAllEmployees() {
+        return new ArrayList<>(employees.values());
+    }
+
+    public List<Employee> filterEmployeeBySalary(double minSalary){
+        return employees.values().stream()
+        .filter(employee -> employee.getSalary().orElse(0.0) >= minSalary)
+        .collect(Collectors.toList());
+    }
+
+    public List<Employee> sortEmployeeBySalary(){
+        return employees.values().stream()
+        .sorted(Comparator.comparingDouble(employee -> employee.getSalary().orElse(0.0)))
+        .collect(Collectors.toList());
+    }
+    
+
+}
